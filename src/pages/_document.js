f8b4094b816357aa1c853abf5824c78697eba2cc
@Lourdes84 <lourdes.gc84@gmail.com>
2023-06-28 16:29:44 +0200

    Fix FOUC bug

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,16 +1,48 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
+const MyDocument = (props) => {
   return (
-    <Html lang="es">
+    <Html>
       <Head />
       <body>
-        <script>0</script>
         <Main />
         <NextScript />
+        {props.styles}
       </body>
     </Html>
   )
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet()
+  const originalRenderPage = ctx.renderPage
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+};
+
+export default MyDocument
+
+
+
 
 
