1c85391753ad56ba48b7aa74d03b4d8cad446ae2
@Lourdes84 <lourdes.gc84@gmail.com>
2023-07-11 18:10:22 +0200

    delete useffect

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,9 @@ import Aside from '@/layout/Aside/Aside'
 import LinksInterest from '@/components/LinksInterest/LinksInterest'
 import SocialMediaBanner from '@/components/SocialMediaBanner/SocialMediaBanner'
 import MainContainer from '@/layout/MainContainer/MainContainer'
-import { useEffect, useState } from 'react'
 
 const Home = () => {
   const isPopular = "Popular"
-
-  const [ isSmallScreen, setIsSmallScreen ] = useState(false)
-
-  useEffect(() => {
-    const handleResize = () => setIsSmallScreen(window.innerWidth < 1840)
-    window.addEventListener('resize', handleResize)
-    handleResize()
-    return () => window.removeEventListener('resize', handleResize)
-}, [] )
-
 
   return (
     <>
@@ -27,7 +16,7 @@ const Home = () => {
         <link rel="icon" href="/tenedor.svg" />
       </Head>
       <>  
-          <div className={isSmallScreen ? "container-small" : "container"}>
+          <div className="container">
               <MainContainer typeOfRecipe={isPopular} />
               <Aside />
           </div>
@@ -49,21 +38,6 @@ const Home = () => {
               @media (min-width: 1071px) {
                 margin-top: 150px;
               }
-            }
-
-            .container-small {
-              display: flex;
-              justify-content: center;
-              gap: 60px;
-
-              @media (max-width: 1840px) {
-                flex-direction: column;
-              }
-
-              @media (min-width: 1071px) {
-                margin-top: 150px;
-              }
-              
             }
         `}
       </style>
